3b5df2cd5855cf0fa9a538d125e1dcb15a2a305d
Ashok <dhunganaashok312@gmail.com>
2023-04-30 16:42:55 +0545

    Mern practice

---
+++ index.js
@@ -0,0 +1,17 @@
+let brands = ["apple","sony","samsung"]
+console.log("brands",brands);
+console.log("element in second position is ",brands[1]);
+let course ={
+    name: "Mern",
+    duration :"2.5",
+
+}
+console.log("course name" , course.name);
+course.duration = 3 
+console.log("course duration",course.duration);
+console.log("course fees",course.fees);
+
+function sum(){
+    return sum;
+}
+sum(4,5);
